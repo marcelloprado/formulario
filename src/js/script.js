51d82f5eea27c38aa82487c5a566a8e427f27860
Marcello Prado <marcellopradomuller@gmail.com>
2022-07-17 10:13:40 -0300

    Round 2 com o JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,75 +1,42 @@
+let form = document.getElementById('form')
 let nome = document.getElementById('nome');
 let email = document.getElementById('email');
 let telefone = document.getElementById('telefone');
 let mensagen = document.getElementById('mensagen');
 
 
-let nomeValue = nome.value
 form.addEventListener('submit', function (event) {
-    if(nomeValue == "") {
-       alert('campo vazio');
-       console.log('campo vazio');
-    } else {
-        
-    }
+    event.preventDefault();
+
+    verificarInputs();
+
 });
 
+function verificarInputs() {
+    const nomeValue = nome.value.trim();
+    const emailvalue = email.value.trim();
+    const telefoneValue = telefone.value.trim();
+    const mensagenvalue = mensagen.value.trim();
+    // trim => serve para retirar os espaços do inicio e do fim do input deixado pelo usuário.
 
+     if(nomeValue === '') {
+       // Mostrar erro
+       // Adicionar a classe error
+       erroValidacao(nome, 'Campo Obrigatório')
+    } else {
+        // adicionar a classe de sucesso
+    }
+}
 
+function erroValidacao(input, message) {
+    // const inputs = input.parentElement;
+    let small = document.querySelector('small')
 
+   nome.classList = 'inputs erro'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.getElementById('form')
-// const nome = document.getElementById('nome')
-// const email = document.getElementById('email')
-// const telefone = document.getElementById('telefone')
-// const mensagen = document.getElementById('mensagen')
-
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-
-//     checkInputs()
-// });
-
-// function checkInputs() {
-
-//     // trim => serve para retirar os espaços do inicio e do fim do input deixado pelo usuário.
-
-//   const nomeValue = nome.Value.trim() 
-//   const emailValue = email.Value.trim()
-//   const telefoneValue = telefone.Value.trim()
-//   const mensagenValue = mensagen.Value.trim()
-
-
-//   if(nomeValue === '') {
-//     // Mostrar erro
-//     // Adicionar a classe error
-
-//     errorValidation(nome, 'Campo obrigatório')
-//   } else {
-
-//   }
-
-// }
+//    small.classList = 'inputs erro small'
+    // small.className = 'inputs erro small'
+   small.innerHTML = (message)
+        console.log(small);
+   
+}
